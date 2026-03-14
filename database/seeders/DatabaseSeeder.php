2ef4e8e825044a942bc65bd18378ca3c8cc20ce7
Reeptto <0204ariaprianto@gmail.com>
2026-03-14 08:01:34 +0700

    Paginating, Filtering, Sorting

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(ProdukSeeder::class);
+        $this->call([
+                ProdukSeeder::class,
+                HardwareSoftwareProdukSeeder::class
+            ]);
     }
 }
